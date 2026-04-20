a0eff6f048a6ef93ca128a8ff6d2bd9e31200970
RyanBrittCUB <rybr6550@colorado.edu>
2026-04-20 13:22:03 -0600

    Update candyLogic.cpp

--- candyLogic.cpp
+++ candyLogic.cpp
@@ -1,11 +1,13 @@
 /* TODO: Initialize candyLogic.cpp */
 #include "candyLogic.h"
+#include <bits/stdc++.h>
+
 
 int candyJarSort(char c, string candies, string targetCandy){
 
 int count=0;
 string testcandy;
-
+//Need to find string version of the getline command
 while(getline(candies, testcandy, ',')){
     if(getline(candies, testcandy, ',')==targetCandy){
         count++;
@@ -13,9 +15,17 @@ while(getline(candies, testcandy, ',')){
 }
 return count;
 }
-string inventoryScrambler(char c, string inventory);
 
 
+
+string inventoryScrambler(char c, string inventory){
+    reverse(inventory.begin(), inventory.end());
+
+    for(int i=0; i<inventory.length(); i=i+2){
+        inventory[i]=inventory[i]-32;
+    }
+
+}
 
 
 
