65184f4be5b42f47eff239a4b11485c1eaa8373b
RyanBrittCUB <rybr6550@colorado.edu>
2026-04-20 14:08:34 -0600

    Boiler plate code complete

--- candyLogic.cpp
+++ candyLogic.cpp
@@ -8,11 +8,22 @@ int candyJarSort(char c, string candies, string targetCandy){
 int count=0;
 string testcandy;
 //Need to find string version of the getline command
-while(getline(candies, testcandy, ',')){
+
+
+/*while(getline(candies, testcandy, ',')){
     if(getline(candies, testcandy, ',')==targetCandy){
         count++;
     }
+}*/
+    int L=candies.length();
+for(int c=0; c < L;c++){
+    if(candies.substr(c, targetCandy.length())==targetCandy){
+        count++;
+    }
 }
+
+
+
 return count;
 }
 
@@ -20,11 +31,12 @@ return count;
 
 string inventoryScrambler(char c, string inventory){
     reverse(inventory.begin(), inventory.end());
+    int L=inventory.length();
 
-    for(int i=0; i<inventory.length(); i=i+2){
+    for(int i=0; i<L; i=i+2){
         inventory[i]=inventory[i]-32;
     }
-
+    return inventory;
 }
 
 
@@ -32,9 +44,7 @@ string inventoryScrambler(char c, string inventory){
 
 
 
-string combineCandies(char c, string candy1, string candy2);
-
-{
+string combineCandies(char c, string candy1, string candy2) {
     int candy1length = candy1.length();
     int candy2length = candy2.length();
     int outputLength = candy1length + candy2length;
@@ -57,4 +67,28 @@ string combineCandies(char c, string candy1, string candy2);
 
 
 
-int findGoldenTicket(char c, string batch, string ticket);
+int findGoldenTicket(char c, string batch, string ticket){
+
+    int idx;
+    int L=batch.length();
+    int lT = ticket.length();
+
+    for(int c=0; c<L; c++){
+        if(batch[c]<91){
+            batch[c]= batch[c]-32;
+        }
+    }
+     for(int c=0; c<lT; c++){
+        if(ticket[c]<91){
+            ticket[c]= ticket[c]-32;
+        }
+    }
+
+    for(int i=0; i<L; i++){
+        if(batch.substr(i, ticket.length()) == ticket){
+            i=idx;
+        }
+    }
+
+    return idx;
+}
