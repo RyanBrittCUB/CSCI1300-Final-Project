40b94704f36716a597191bec1148a2481cce30af
RyanBrittCUB <rybr6550@colorado.edu>
2026-04-21 18:43:21 -0600

    Update testCandyLogic.cpp

--- testCandyLogic.cpp
+++ testCandyLogic.cpp
@@ -1,1 +1,15 @@
 /* TODO: Complete testCandyLogic.cpp */
+#include <iostream>
+#include "candyLogic.h"
+using namespace std;
+
+int main(){
+    string batch="taffy,sour,sweet,chocolate,mike,ike,taffy,taffy";
+    string targetCandy="taffy";
+    int count = candyJarSort(char c, string batch, string targetCandy);
+
+    
+
+
+
+}
